77e23593087c4c0216693272ab65a1fedd7d9560
Praise Paul <praise20024christ@gmail.com>
2026-09-15 13:35:48 +0530

    Add bills indexes

--- server/src/db.ts
+++ server/src/db.ts
@@ -19,6 +19,8 @@ export async function connectDb(): Promise<Db> {
     db.collection('expenses').createIndex({ expenseNumber: 1 }, { unique: true }),
     db.collection('labour').createIndex({ labourDate: -1, createdAt: -1 }),
     db.collection('labour').createIndex({ labourNumber: 1 }, { unique: true }),
+    db.collection('bills').createIndex({ isPaid: 1, dueDate: 1, createdAt: -1 }),
+    db.collection('bills').createIndex({ billNumber: 1 }, { unique: true }),
   ])
   return db
 }
